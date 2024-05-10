07d805d87116386a88fbbb9c43a947dcaded3449
diane <villalino.136534100519@depedqc.ph>
2024-05-10 11:22:43 +0800

    Fuction of the Calculator

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -18,5 +18,14 @@ def divide(first_number, second_number):
         return "Error: Division by zero is not allowed."
 
 
+def calculator():
+    while True:
+        print("\n\033[3mSimple App Calculator")
+        print("\n\033[1;95mChoose an operation:")
+        print("\033[0;36m1. Addition")
+        print("\033[0;36m2. Subtraction")
+        print("\033[0;36m3. Multiplication")
+        print("\033[0;36m4. Division")
+        
 # Define calculator function
 # Loop until user opts to exit
