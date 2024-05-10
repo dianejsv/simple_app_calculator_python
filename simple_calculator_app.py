a42bf8e74a84f18dc0209415037c8aa49a4cf9de
diane <villalino.136534100519@depedqc.ph>
2024-05-10 13:39:36 +0800

    Pseudocode

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -18,6 +18,7 @@ def divide(first_number, second_number):
         return "Error: Division by zero is not allowed."
 
 
+# Define calculator functions
 def calculator():
     while True:
         print("\n\033[3mSimple App Calculator")
@@ -26,7 +27,7 @@ def calculator():
         print("\033[0;36m2. Subtraction")
         print("\033[0;36m3. Multiplication")
         print("\033[0;36m4. Division")
-
+        # Get the user's choice
         choice_operation = input("\n\033[0;34mEnter choice (1/2/3/4): ")
         if choice_operation in ("1", "2", "3", "4"):
             while True:
@@ -36,7 +37,7 @@ def calculator():
                     break
                 except ValueError:
                     print("Invalid input. Please enter numbers only.")
-
+            # Operators performing their functions
             if choice_operation == "1":
                 print("\n\033[31mResult: {}\033[0m".format(add(first_number, second_number)))
             elif choice_operation == '2':
@@ -51,7 +52,7 @@ def calculator():
                     print("Result: \n\033[31m{}\033[0m".format(result))
         else:
             print("Invalid choice. Please enter a valid option.")
-
+        # Ask the user if they want to try again
         while True:  # This loop ensures input is either yes or no
             again = input("Do you want to try again? (yes/no): ")
             if again.lower() in ('yes', 'y'):
@@ -63,4 +64,5 @@ def calculator():
                 print("Invalid choice. Please choose between yes or no.")
 
 
+# Run the Calculator
 calculator()
