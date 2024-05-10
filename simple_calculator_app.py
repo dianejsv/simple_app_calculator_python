ac1ee5541e5a8a8e51c33b8aea5c45377e83a13f
diane <villalino.136534100519@depedqc.ph>
2024-05-10 10:49:58 +0800

    Addition operator function

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -1,3 +1,7 @@
 # Define function of each operation
+def add(first_number, second_number):
+    return int(first_number + second_number)
+
+
 # Define calculator function
 # Loop until user opts to exit
