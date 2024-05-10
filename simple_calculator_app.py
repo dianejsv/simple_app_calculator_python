60075c72b85adb25ee5acdca918492c49901105f
diane <villalino.136534100519@depedqc.ph>
2024-05-10 10:54:19 +0800

    Subtraction operator function

--- simple_calculator_app.py
+++ simple_calculator_app.py
@@ -3,5 +3,9 @@ def add(first_number, second_number):
     return int(first_number + second_number)
 
 
+def subtract(first_number, second_number):
+    return int(first_number - second_number)
+
+
 # Define calculator function
 # Loop until user opts to exit
